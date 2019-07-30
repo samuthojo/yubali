be2726943175f7693b278fb95d5e6a95b278a996
samuthojo <samuthojo@gmail.com>
2019-07-30 14:31:37 +0300

    modified:   config/database.php

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -58,11 +58,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', parse_url(getenv('DATABASE_URL'), 1)),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', substr(parse_url(getenv('DATABASE_URL'), 5),1)),
+            'username' => env('DB_USERNAME', parse_url(getenv('DATABASE_URL'), 3)),
+            'password' => env('DB_PASSWORD', parse_url(getenv('DATABASE_URL'), 4)),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
