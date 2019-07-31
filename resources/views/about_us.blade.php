f0ea62f1ac96aa21ab6da5afc21d843a8dee532b
samuthojo <samuthojo@gmail.com>
2019-07-31 09:25:37 +0300

    public/uploads/1/5d40a740a6a79jpg

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -4,20 +4,41 @@
   
 <div class="col-xs-12">
   
+  <h3>About</h3>    
+  <p>The Foundation is a skilled, talented and professional’s knowledge based social organization. It is non-profit making organization convinced that there shall be or exist common interests among members which ought to be explored, promote and self-guarded for mutual advantage.</p>
+  
+  <p>This organization being essentially knowledge based service delivery among others things it will focus on addressing the problem of poverty that will help improve the employment outlook for promoting the upcoming gospel singers since:- We believe that new and more aggressive strategies – supported by state but implemented through multi-stakeholder partnerships – are required to empower more singers to succeed in the new socio-economic and technological opportunities and utilization of their skills in true gospel process are of paramount important. </p>
+  
+  <p>The foundation is determined to promote gospel singers industry for the benefit of the whole population for being the platform of discovery and musical technological growth among the singers to make sure they curb the consequences of job scarcity, social tensions, and to provide recruitment solutions for our members, to find job opportunities for our candidates, and to build employability and address skills shortages through workforce development initiatives.</p>
+  
+  <p>We also aim at facilitating the church community to partner directly with us through providing trained, singers as full ministers in the gospel music industry as the middle men to connect singers with a church to remove the barrier that is keeping increasing by singers to charge the church a certain amount of money for them to minister while they are the part of the Christ’s body. We use platforms such as kipaji app to meet our singers’ financial needs and be able to minister to the church community and all other non-profit events free of charge to build the kingdom of our Lord Jesus Christ.</p>
+  
+  <h3>Background</h3>
+  <p>A lot has happened in the gospel music industry over the years. Some of the choir groups that kept the gospel music scene live have lost their popularity while others have faded into oblivion. Individuals have emerged and changed the status of the gospel music. There may still be a long list of up-and-coming gospel music artists but the status of gospel music in the country seems to lose its popularity in recent years.</p>
+  
+  <p>A recent survey with several key players in Tanzania’s gospel industry reveals some tentative answers as to why the East African nation’s once-thriving gospel scene seems to be in a state of rapid decline. Gospel music as we know it today started in Tanzania back in the 1980s. Back then, only choir groups and bands were responsible for the so-called full evangelical ministry, preaching through songs. But the presentation of music through choirs worked against the genre and by the mid-1990s gospel music was not a genre that moved the masses.</p>
+  
+  <p>The genre was underrated and anyone venturing into this area of music was frowned upon or regarded as a mediocre. It was difficult to hear this kind of music playing at events, or even in public places as the genre simply became unattractive to many think it is easy to make money under the umbrella of gospel music, and as a result many people whose music has never even been heard go around claiming to be gospel artists.</p>
+  
+  <h3>Problem Statement and Justification</h3>
+  <p>The gospel industry might be big but as revealed by views of the stakeholders, the numerous challenges it is undergoing impedes its growth and demand its place on the global music scene. Treating gospel music just like any other type of music is regarded as one of the main reason for the decline of the industry that has struggled to get the attention of audiences in the past. The survey further reveals that the mushrooming of gospel music singers in Tanzania is due to lack of jobs.</p>
+  
+  <p>On other hands some promoters and media houses arguably play a big role in killing the gospel music industry in Tanzania. Others accused media practitioners for being unprofessional and asking for money from them in order to promote their work. Despite its rapid rise to popularity since the late 1990s, the many challenges currently facing gospel artists in Tanzania threaten to derail the genre's progress.</p>
+  
+  <p>Then it’s totally agreed that gospel music singers play a very unique and important role of preaching the word of God. But once they achieve substantial media publicity, these artists tend to think they have already made it in the industry. It is no wonder the new artists only last a few years.</p>
+  
+  <p>More importantly, it should be the core duty of this foundation to awake gospel singers and musical practionioners to be trained and use their skills and professionalism on making sure that they must play the role of being active participants in true gospel progress and bringing their bright future, despite of being vulnerable with their work to be of low standards nicked as “it is about favors and not about the quality of work, it is possible then to maintain the essence of their talents.</p>
+  
+  <p>It is recommended that singers should join effort and come together on seizing opportunities and use their skills and talents to develop themselves by becoming active and informed about their potentialities they can increase their own opportunities for development and to build employability, increase experiences and address skills shortages through workforce development initiatives propounded by this foundation.</p>
+  
   <h3>Vision</h3>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <p>To give hope to the hopeless with outstanding skills and talents by developing the great minds for great bright future and viable gospel society.</p>
   
   <h3>Mission</h3>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <p>To stimulate and promote effective use of talents and skills among members on addressing Socio-Cultural, Economical; Environmental and technological issues/challenges in the nation.</p>
   
-  <h3>Objectives</h3>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+
   
 </div>
 
